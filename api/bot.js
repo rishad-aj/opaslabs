98e7a711e7ce5138f71482013fab6fc496b6d892
rishadaj874 <rishu.rishad2019@gmail.com>
2025-07-31 22:46:08 +0530

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,26 +2,26 @@
 
 import TelegramBot from 'node-telegram-bot-api';
 
-let bot;
-
-if (!bot) {
+if (!global.bot) {
   const token = process.env.TELEGRAM_TOKEN;
   const webhookUrl = `https://${process.env.VERCEL_URL}/api/bot`;
 
-  bot = new TelegramBot(token, { webHook: true });
+  const bot = new TelegramBot(token, { webHook: true });
   bot.setWebHook(webhookUrl);
-}
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `Your chat ID is: \`${chatId}\``, {
-    parse_mode: 'Markdown',
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `your Chat ID is: \`${chatId}\``, {
+      parse_mode: 'Markdown',
+    });
   });
-});
+
+  global.bot = bot;
+}
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    bot.processUpdate(req.body);
+    global.bot.processUpdate(req.body);
     res.status(200).end();
   } else {
     res.status(200).send("🤖 Telegram bot webhook is active!");
